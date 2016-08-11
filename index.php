4adc2ef893438a84329a9c9c83d6e38a33b71624
Zain ul abidin <zain@aligent.com.au>
2016-08-11 15:31:19 +0930

    Added comment which apis are not working

--- index.php
+++ index.php
@@ -11,13 +11,28 @@ $dotenv->load();
 $toggl_token = $_ENV['TOGGLE_API_KEY'];
 
 $client = new GuzzleHttp\Client();
-$vUrl = 'https://www.toggl.com/api/v8/workspaces/1043423/tasks';
 $vUrl = 'https://www.toggl.com/api/v8/workspaces';
+$vUrl = 'https://www.toggl.com/api/v8/workspaces/1043423';
+$vUrl = 'https://www.toggl.com/api/v8/workspaces/1043423/users';
+
+/**
+ * Following won't work because it is only available for pro
+ */
+$vUrl = 'https://www.toggl.com/api/v8/workspaces/1043423/tasks';
+//$vUrl = 'https://www.toggl.com/api/v8/workspaces/1043423/projects';
+/**
+ * Following is not working for some reason.
+ * I assigned project to multiple tasks but no task is appearing
+ */
+$vUrl = 'https://www.toggl.com/api/v8/projects/10764415/tasks';
+
+$vUrl = 'https://www.toggl.com/api/v8/projects/10764415';
 $res = $client->get($vUrl, ['auth' => [$toggl_token, 'api_token']]);
-echo $res->getStatusCode();
+//echo $res->getStatusCode();
 // "200"
-echo $res->getHeader('content-type');
+//echo $res->getHeader('content-type');
 // 'application/json; charset=utf8'
-echo $res->getBody();
+echo "<pre>";
+print_r($res->getBody());
 // {"type":"User"...'
 var_export($res->json());
